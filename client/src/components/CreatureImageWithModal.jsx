8611a540a9e265582c5a69e5d1fbd2ac4f98b1a4
Vsevolod Epinetov <vsevolod@epinetov.com>
2023-05-04 20:07:15 +0300

    feat: added new component for a cart page

--- client/src/components/CreatureImageWithModal.jsx
+++ client/src/components/CreatureImageWithModal.jsx
@@ -7,7 +7,7 @@ const CreatureImageWithModal = ( {imageSource, miniName} ) => {
 
   return (
     <>
-      <Image maw={50} mx="auto" radius="md" src={imageSource} alt={`Превьюшка модельки ${miniName}`} onClick={(e) => { handlersModalImage.open(); }} style={{ cursor: 'pointer' }} />
+      <Image maw={100} mx="auto" radius="md" src={imageSource} alt={`Превьюшка модельки ${miniName}`} onClick={(e) => { handlersModalImage.open(); }} style={{ cursor: 'pointer' }} />
       <Modal opened={modalOpenedImage} onClose={() => handlersModalImage.close()} title='Превью' centered size='lg' style={{ padding: '45px' }}>
         <Image mx="auto" radius="md" src={imageSource} alt={`Превьюшка миньки ${miniName}`} />
       </Modal>
